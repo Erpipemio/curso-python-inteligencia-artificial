2f1bb97570a6ea9498eb755c55abaacaee448375
Dy2g <dy2g69@gmail.com>
2025-06-24 21:02:38 -0400

    mini ejercicios del dia 4

---
+++ Dia_4/mini_ejercicios/ejercicios.py
@@ -0,0 +1,37 @@
+# #LISTAS
+# Crea una lista con 5 cosas que te gustan.
+# Imprime solo la tercera.
+# Cámbiala por otra cosa.
+# Elimina la primera.
+
+# cosas = ["Pizza", "Cine", "Leer", "Viajar", "Música"]
+# print("Lista de cosas que me gustan:", cosas)
+# print("Tercera cosa que me gusta:", cosas[2])
+# cosas[2] = "Jugar videojuegos"
+# print("Lista después de cambiar la tercera cosa:", cosas)
+# del cosas[0]
+# print("Lista después de eliminar la primera cosa:", cosas)
+
+# Diccionarios
+# Crea un diccionario con:
+# "nombre", "edad", "moto"
+# Cambia "moto" por el nombre de tu club.
+# Agrega una clave "activo" con el valor True.
+
+# Yo = {"nombre": "Diego", "edad": 33, "moto": "Br200"}
+# print("Diccionario original:", Yo)
+# Yo["moto"] = "TeamBr200"
+# Yo["activo"] = True
+# print("Diccionario actualizado:", Yo)
+
+# Sets
+# Crea un set con 3 cosas que odias.
+# Agrega una más.
+# Intenta agregar un duplicado.
+# Imprime todos los elementos.
+
+cosas_odiosas = {"Tráfico", "Esperar", "ordenar"}
+print("Set original:", cosas_odiosas)
+cosas_odiosas.add("Colas")
+cosas_odiosas.add("Tráfico")  # Intento de duplicado
+print("Set después de agregar una cosa más:", cosas_odiosas)
